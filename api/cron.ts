4aedbc115d6e4bd0ea12274e729bc45cb1bd01bd
davidabdel <davidabdel@users.noreply.github.com>
2026-05-19 11:16:49 +1000

    Completely isolate Vite from serverless endpoints to fix Vercel build

--- api/cron.ts
+++ api/cron.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { pollCliniko } from "../server";
+import { pollCliniko } from "../app";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
